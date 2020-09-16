848350390e618ae80b5d37977c312f82b12a95e5
imamfahr <imamfahr@gmail.com>
2020-09-16 21:37:35 +0700

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
             <h1>React #7</h1>
             <nav>
             <ul>
-                <div><Link to ='/'>Home</Link>></div>
+                <div><Link to ='/'>Home</Link></div>
                 <div><Link to='/regist'>Regsiter Form</Link></div>
                 <div><Link to='/to-uppercase'></Link></div>
                 <div><Link to='/bmi-calculator'></Link></div>
